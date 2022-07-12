432d6c854497e58a98369e269b3f3acb3c2c5002
meghanaayyala <meghanaayyalasomayajula@gmail.com>
2022-07-12 15:12:22 +0530

    updated files

--- store/models/product.py
+++ store/models/product.py
@@ -12,7 +12,6 @@ class Product(models.Model):
     # added bestsellerstatus
     bestsellerstatus=models.CharField(max_length=50,default='',null=True,blank=True)
     image = models.ImageField(upload_to='uploads/products/')
-
     @staticmethod
     def get_products_by_id(ids):
         return Product.objects.filter(id__in =ids)
